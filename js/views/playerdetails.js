419dca53beda5cdf56e56896122e5ec5fcbc83f5
bmichaelis <brett.michaelis@gmail.com>
2013-05-28 20:11:59 -0600

    More cleanup and fixed the templates that were calling for non-existent variables

--- js/views/playerdetails.js
+++ js/views/playerdetails.js
@@ -3,6 +3,7 @@ directory.PlayerView = Backbone.View.extend({
     render: function () {
         this.$el.html(this.template(this.model.attributes));
         $('#details', this.el).html(new directory.PlayerSummaryView({model:this.model}).render().el);
+/*
         this.model.reports.fetch({
             success:function (data) {
                 if (data.length == 0)
@@ -10,6 +11,7 @@ directory.PlayerView = Backbone.View.extend({
             }
         });
         $('#reports', this.el).append(new directory.PlayerListView({model:this.model.reports}).render().el);
+*/
         return this;
     }
 });
